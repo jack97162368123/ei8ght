567081719537e7c2c04030049efbba33c27ddbba
Jack Robetson <jackrobertsonps3@gmail.com>
2023-05-31 17:56:03 +0100

    Mobile opertimisations

--- src/components/EightComponent.js
+++ src/components/EightComponent.js
@@ -2,13 +2,13 @@ import React from 'react';
 import '../styles/WelcomeEight.css';
 
 const EightComponent = () => {
-    return (
-        <div>
-            <div className="eight-text">
-            <p>EI8HT</p>
-            </div>
-            </div>
-    );
-}
+  return (
+    <div className="container">
+      <div className="eight-text">
+        <p>EI8HT</p>
+      </div>
+    </div>
+  );
+};
 
 export default EightComponent;
