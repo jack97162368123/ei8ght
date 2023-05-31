c1cc86c5e1b64d04947724c9111a855f56a94448
Jack Robetson <jackrobertsonps3@gmail.com>
2023-05-31 17:17:25 +0100

    imageupdates

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/static/`
+        path: `${__dirname}/static/`
       },
     },
   ],
