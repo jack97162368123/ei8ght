202912aa344f2a3e81954d57da58510e226db003
Jack Robetson <jackrobertsonps3@gmail.com>
2023-05-31 15:39:12 +0100

    Commit 4

--- src/components/Bg.js
+++ src/components/Bg.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../styles/Bg.css';
+import bgImage from '../images/Bg.svg';
 
 const Bg = () => {
+  const bgStyle = {
+    backgroundImage: `url(${bgImage})`,
+  };
+
   return (
     <div className="bg-container">
-      <div className="bg"></div>
+      <div className="bg" style={bgStyle}></div>
     </div>
   );
 };
