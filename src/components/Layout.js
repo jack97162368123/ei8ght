c78867d924bc11d7e9c65be86f55baf96711a1f0
Jack Robetson <jackrobertsonps3@gmail.com>
2023-08-04 08:09:28 +0100

    Nav bar and loading component changes to make the nav apear aboce and the loading animation ontop of all content

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import Loading from './loading/Loading'; // Make sure this path is correct
+import React, { useState, useEffect } from "react";
+import Loading from "./loading/Loading"; // Make sure this path is correct
 import Bg from "./Bg";
 import NavBar from "./NavBar";
 import Paragraph from "./Paragraph";
@@ -33,7 +33,12 @@ import Swote_Top from "../images/TopImages/Swote_Top.png";
 
 const words = ["Share", "Connect", "Learn"];
 
-const Layout = ({ paragraphColumn = "left", titleColumn = "left", imageColumn = "left", children }) => {
+const Layout = ({
+  paragraphColumn = "left",
+  titleColumn = "left",
+  imageColumn = "left",
+  children,
+}) => {
   const [isLoading, setIsLoading] = useState(true);
   const [opacity, setOpacity] = useState(1);
 
@@ -41,11 +46,11 @@ const Layout = ({ paragraphColumn = "left", titleColumn = "left", imageColumn = 
     // Minimum duration timer
     const minimumDurationTimer = setTimeout(() => {
       // If the page has already loaded, start the fade-out
-      if (document.readyState === 'complete') {
+      if (document.readyState === "complete") {
         startFadeOut();
       } else {
         // If the page hasn't loaded yet, wait for the load event
-        window.addEventListener('load', startFadeOut);
+        window.addEventListener("load", startFadeOut);
       }
     }, 2000); // Minimum duration of 2 seconds
 
@@ -57,131 +62,139 @@ const Layout = ({ paragraphColumn = "left", titleColumn = "left", imageColumn = 
     };
 
     return () => {
-      window.removeEventListener('load', startFadeOut);
+      window.removeEventListener("load", startFadeOut);
       clearTimeout(minimumDurationTimer);
     };
   }, []);
 
   return (
-    <div className="layout">
-      {isLoading && <Loading opacity={opacity} />}
-
-          <Seo
-            title="Eight PR & Marketing - Creating Empathetic and Creative Experiences"
-            description="EIGHT PR & Marketing is a specialist agency with a mission to blend business with pleasure in the realm of events and experience marketing. Our focus is empathetic marketing and creative campaigns that connect deeply with audiences. EIGHT is more than a number, it's a club, a network, and a promise of quality and connection."
-          />
-          <Bg />
-          <NavBar />
-          <main className="content">
-            <WelcomeEightComponent welcomeText="Welcome to" contentText="EI8HT" />
-
-      <div className="grid-container">
+    <>
+    {isLoading && (
+      <div className="loading-component" style={{ opacity: opacity }}>
+        <Loading />
+      </div>
+    )}
+    
+      <div className="layout">
       
 
-        <div className="full-width-column" colSpan="2">
-          <FullWidthComponent svgImage={svgImage1} />{" "}
-        </div>
+      <Seo
+        title="Eight PR & Marketing - Creating Empathetic and Creative Experiences"
+        description="EIGHT PR & Marketing is a specialist agency with a mission to blend business with pleasure in the realm of events and experience marketing. Our focus is empathetic marketing and creative campaigns that connect deeply with audiences. EIGHT is more than a number, it's a club, a network, and a promise of quality and connection."
+        />
+        <NavBar />
+      <Bg />
+      
+      
+      <main id="home" className="content">
+        <WelcomeEightComponent welcomeText="Welcome to" contentText="EI8HT" />
 
-        <div className="full-width-column" colSpan="2">
-          <VerticalScrollCarousel words={words} />{" "}
-        </div>
-
-        {paragraphColumn === "left" && (
-          <div className="left-column">
-            <Paragraph text="A specialist PR & marketing agency working in the world of events and experience marketing. We exist because we love events, marketing, and live experiences. <br /> We talk about empathetic marketing; the creation of campaigns that take the time to understand audiences, their world, and their lives, and that build a closer connection with them. <br /> We talk about creativity; creating ideas, campaigns, and messages that create empathy and action.\n We exist in that sweet spot between events and brands, people and places, emotion, creativity, and intelligence." />
+        <div className="grid-container">
+          <div className="full-width-column" colSpan="2">
+            <FullWidthComponent svgImage={svgImage1} />{" "}
           </div>
-        )}
 
-        {imageColumn === "left" && (
-          <div className="right-column">
-            <ImageComponentHalf src={Happy} alt="Alistair Turner" />
+          <div className="full-width-column" colSpan="2">
+            <VerticalScrollCarousel words={words} />{" "}
           </div>
-        )}
 
-        <div className="full-width-column">
-          <FullWidthComponent svgImage={svgImage2} />
-        </div>
+          {paragraphColumn === "left" && (
+            <div className="left-column">
+              <Paragraph text="A specialist PR & marketing agency working in the world of events and experience marketing. We exist because we love events, marketing, and live experiences. < br /> We talk about empathetic marketing; the creation of campaigns that take the time to understand audiences, their world, and their lives, and that build a closer connection with them. <br /> We talk about creativity; creating ideas, campaigns, and messages that create empathy and action. We exist in that sweet spot between events and brands, people and places, emotion, creativity, and intelligence." />
+            </div>
+          )}
 
-        {imageColumn === "left" && (
-          <div className="left-column">
-            <ImageComponentHalf src={svgImage5} alt="Alistair Turner" />
+          {imageColumn === "left" && (
+            <div className="right-column">
+              <ImageComponentHalf src={Happy} alt="Alistair Turner" />
+            </div>
+          )}
+
+          <div className="full-width-column">
+            <FullWidthComponent svgImage={svgImage2} />
           </div>
-        )}
 
-        {paragraphColumn === "left" && (
-          <div className="right-column">
-            <Paragraph
-              text="EIGHT started with a simple mission; to create a business that mixes business and pleasure. We attend events because they are productive, hard work, but great fun. This is allowed. You are allowed to enjoy your work.<br />
+          {imageColumn === "left" && (
+            <div className="left-column">
+              <ImageComponentHalf src={svgImage5} alt="Alistair Turner" />
+            </div>
+          )}
+
+          {paragraphColumn === "left" && (
+            <div className="right-column">
+              <Paragraph
+                text="EIGHT started with a simple mission; to create a business that mixes business and pleasure. We attend events because they are productive, hard work, but great fun. This is allowed. You are allowed to enjoy your work.<br />
             
               EIGHT was formed out of a belief that this is what business should be like; that if you love what you do, you will be good at it, and it will be good for everyone; colleagues, clients, people, planet. <br />
               
               EIGHT was built around connections; a club mentality. Our first followers are our clients, then our network of stakeholders who formed the rest of our club. <br />
               
               So why EIGHT? Because we like circles but we also like connections. Because EIGHT is a lucky number, and we’re the lucky ones; we love what we do. EIGHT isn’t just a number to us; it’s a club, a network, and a promise."
-            />
+              />
+            </div>
+          )}
+
+          <div className="full-width-column" colSpan="2">
+            <FullWidthComponent svgImage={meetTheTeamImage} />
           </div>
-        )}
 
-        <div className="full-width-column" colSpan="2">
-          <FullWidthComponent svgImage={meetTheTeamImage} />
-        </div>
+          {imageColumn === "left" && (
+            <div className="left-column">
+              <ImageComponentHalf
+                src={Swote_Top}
+                alt="Qwote saying IF YOU WANT TO LIFT YOURSELF UP LIFT UP SOMEONE ELSE Booker T Washington"
+              />
+            </div>
+          )}
 
-        {imageColumn === "left" && (
-          <div className="left-column">
-            <ImageComponentHalf
-              src={Swote_Top}
-              alt="Qwote saying IF YOU WANT TO LIFT YOURSELF UP LIFT UP SOMEONE ELSE Booker T Washington"
-            />
+          {imageColumn === "left" && (
+            <div className="right-column">
+              <ImageComponentHalf
+                src={TeamImage}
+                alt="Meat the Team Image (Tug)"
+              />
+            </div>
+          )}
+
+          <div className="full-width-column" colSpan="2">
+            <ThreeColumnImageComponent />
           </div>
-        )}
 
-        {imageColumn === "left" && (
-          <div className="right-column">
-            <ImageComponentHalf
-              src={TeamImage}
-              alt="Meat the Team Image (Tug)"
-            />
+          <div className="full-width-column" colSpan="2">
+            <FullWidthComponent svgImage={svgImage4} />{" "}
           </div>
-        )}
 
-        <div className="full-width-column" colSpan="2">
-          <ThreeColumnImageComponent />
-        </div>
+          <div className="full-width-column" colSpan="2">
+            <ExperienceLogos />
+          </div>
 
-        <div className="full-width-column" colSpan="2">
-          <FullWidthComponent svgImage={svgImage4} />{" "}
-        </div>
+          <div id="design-branding" className="full-width-column" colSpan="2">
+            <DesignBranding />
+          </div>
 
-        <div className="full-width-column" colSpan="2">
-          <ExperienceLogos />
-        </div>
+          <div id="philosophy" className="full-width-column">
+            <Philosphy />
+          </div>
 
-        <div id="design-branding" className="full-width-column" colSpan="2">
-          <DesignBranding />
-        </div>
+          <div id="About" className="full-width-column" colSpan="2">
+            <FullWidthComponent svgImage={About_EIGHT} />{" "}
+          </div>
 
-        <div id="philosophy" className="full-width-column">
-          <Philosphy />
-        </div>
+          <div className="full-width-column" colSpan="2">
+            <ThreeColumnGrid />
+          </div>
 
-        <div id="About" className="full-width-column" colSpan="2">
-          <FullWidthComponent svgImage={About_EIGHT} />{" "}
-        </div>
-
-        <div className="full-width-column" colSpan="2">
-          <ThreeColumnGrid />
-        </div>
-
-        <div className="full-width-column" colSpan="2">
-          <div className="layout">
-            <main>{children}</main>
-            <Footer />
+          <div className="full-width-column" colSpan="2">
+            <div className="layout">
+              <main>{children}</main>
+              <Footer />
+            </div>
           </div>
         </div>
-      </div>
       </main>
-    </div>
-  );
+      </div>
+
+    </>      );
 };
 
 export default Layout;
