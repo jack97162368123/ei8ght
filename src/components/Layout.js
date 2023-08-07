e91125fc55da092e1b914622c84d44cabc39c2cc
Jack Robetson <jackrobertsonps3@gmail.com>
2023-08-07 20:44:12 +0100

    Changes to the logo to build

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -76,12 +76,12 @@ const Layout = ({
       </div>
       )}
       
-     {/* (
+     (
       <div className="coming-soon-component" style={{ position: 'fixed', width: '100%', height: '100%', zIndex: 9999 }}>
         <CommingSoon />
       </div>
     )
-     */}
+    
       <div className="layout">
       
 
