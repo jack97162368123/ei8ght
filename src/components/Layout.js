9d9f836597af599620de48a13ef712fb7abfac98
Jack Robetson <jackrobertsonps3@gmail.com>
2023-08-08 07:43:01 +0100

    Eight

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -76,11 +76,11 @@ const Layout = ({
       </div>
       )}
       
-     (
+     {/* (
       <div className="coming-soon-component" style={{ position: 'fixed', width: '100%', height: '100%', zIndex: 9999 }}>
         <CommingSoon />
       </div>
-    )
+    ) */}
     
       <div className="layout">
       
